a3cd2253c95c6f28899c790d558224ff3a7eacad
chamithg911-afk <chamithg911@gmail.com>
2026-06-13 14:18:36 +0530

    testing fixes for blank screen

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,11 +1,15 @@
 console.log("JS loaded");
 
 const orbit = document.getElementById("orbit");
-const profile = document.getElementById("profile");
 
-if (!orbit) {
-  console.error("Orbit div missing in HTML");
-} else {
-  orbit.innerHTML = "";
-  console.log("Orbit exists ✔");
+if (orbit) {
+  orbit.innerHTML = `
+    <div class="card" style="position:absolute; left:50%; top:50%; transform:translate(-50%, -50%);">
+      <img src="https://via.placeholder.com/80" />
+      <div class="name">Test User</div>
+      <div class="role">Developer</div>
+    </div>
+  `;
 }
+
+console.log("Test card rendered");
